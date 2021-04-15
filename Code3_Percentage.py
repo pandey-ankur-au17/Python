b94597a50dc9cd73f4e80086b90e81aac6731790
pandey-ankur-au17 <ankurpandey131@gmail.com>
2021-04-16 01:29:02 +0530

    Marks and Percentage

---
+++ Code3_Percentage.py
@@ -0,0 +1,18 @@
+#input marks of 3 subjects
+
+a=int(input("Enter marks of subj1:"))
+b=int(input("Enter marks of subj2:"))
+c=int(input("Enter marks of subj3:"))
+
+#addition
+
+d=a+b+c
+
+#percentage calculation
+
+e=d/3
+
+#printing of marks and percentage
+
+print("Your total obtained marks is:",d)
+print("Your obtained percentage is:",int(e),"%")
