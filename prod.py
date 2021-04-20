cdaadd2097b58d08a78c06b4b9190b907c5aa413
pandey-ankur-au17 <ankurpandey131@gmail.com>
2021-04-20 17:44:23 +0530

    prod problem solved

---
+++ prod.py
@@ -0,0 +1,10 @@
+#Given two integer numbers return their product. If the product is greater than 1000, then return their sum
+x=int(input())
+y=int(input())
+
+prod=x*y
+if prod>1000:
+    print(x+y)
+else:
+    print(prod)
+    
